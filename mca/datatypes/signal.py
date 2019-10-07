151de4103e74feb5b8cb13b9448c62d0bda5072c
Kevin Koch <kevink2@mail.uni-paderborn.de>
2019-10-07 11:28:50 +0200

    Rename x to abscissa and y to ordinate

--- mca/datatypes/signal.py
+++ mca/datatypes/signal.py
@@ -7,53 +7,53 @@ class Signal:
     The class describes a signal with two axes and its meta data. The ordinate
     is stored in a one dimensional numpy array. Since this data type
     only allows equidistant sampled signals the abscissa
-    can be described with 3 parameters: starting point, amount of values and
-    the sampling rate.
+    can be described with 3 parameters: starting point (abscissa_start),
+    amount of values (values) and the sampling rate (increment).
     
     Attributes:
         meta_data: Meta data of the signal with quantity, symbol and unit
             for abscissa- and ordinate-axis.
-        start_a (float): Starting point of the signal.
-        values_a (int): Amount of values the signal contains.
-        delta_a (float): Increment between two values.
+        abscissa_start (float): Starting point of the signal.
+        values (int): Amount of values the signal contains.
+        increment (float): Increment between two values.
         ordinate : Ordinate stored in a numpy_array.
     """
 
-    def __init__(self, meta_data, start_a, values_a, delta_a, ordinate):
+    def __init__(self, meta_data, abscissa_start, values, increment, ordinate):
         """Initialize Signal.
         
         Args:
             meta_data: Meta data of the signal with quantity, symbol and unit
-            for abscissa- and ordinate-axis.
-            start_a (float): Starting point of the signal.
-            values_a (int): Amount of values the signal contains.
-            delta_a (float): Increment between two values.
+                for abscissa and ordinate.
+            abscissa_start (float): Starting point of the signal.
+            values (int): Amount of values the signal contains.
+            increment (float): Increment between two values.
             ordinate : Ordinate stored in a numpy_array.
         """
         self.meta_data = meta_data
-        self.start_a = x_start
-        self.x_values = x_values
-        self.delta_x = delta_x
-        self.y = y
+        self.abscissa_start = abscissa_start
+        self.values = values
+        self.increment = increment
+        self.ordinate = ordinate
 
 
-@dataclass  #x and y neuer name
+@dataclass
 class MetaData:
     """Meta data class for the :class:`.Signal` class.
 
     Attributes:
         name (str): Name of the Signal.
-        x_quantity (str): Quantity of the x-axis.
-        x_symbol (str): Symbol of the x-axis.
-        x_unit (str): Unit for the x-axis.
-        y_quantity (str): Quantity of the y-axis.
-        y_symbol (str): Symbol of the y-axis.
-        y_unit (str): Unit for the y-axis.
+        quantity_a (str): Quantity of the abscissa.
+        symbol_a (str): Symbol of the abscissa.
+        unit_a (str): Unit for the abscissa.
+        quantity_o (str): Quantity of the ordinate.
+        symbol_o (str): Symbol of the ordinate.
+        unit_o (str): Unit for the ordinate.
     """
     name: str
-    x_quantity: str
-    x_symbol: str
-    x_unit: str
-    y_quantity: str
-    y_symbol: str
-    y_unit: str
+    quantity_a: str
+    symbol_a: str
+    unit_a: str
+    quantity_o: str
+    symbol_o: str
+    unit_o: str
