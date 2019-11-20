2fdd1b7774f187430ab4a1d4a0ad46c3ca5102ac
Kevin Koch <kevink2@mail.uni-paderborn.de>
2019-11-20 13:28:50 +0100

    Add test class for block testing.

--- tests/test_absolute.py
+++ tests/test_absolute.py
@@ -3,26 +3,15 @@ from mca.block import absolute
 from mca.base import block_base
 from mca.datatypes import signal
 import numpy as np
-
-
-class TestBlock(block_base.Block):
-    def __init__(self, data):
-        super().__init__()
-        self._new_output(meta_data=None)
-        self.outputs[0].data = signal.Signal(None, None, None, None, data)
-
-    def _process(self):
-        pass
+import test_data
 
 
 @pytest.mark.parametrize("test_input", [-5, 1, np.arange(-1, 10)])
 def test_absolute_basic(test_input):
     a = absolute.Absolute()
-    b = TestBlock(test_input)
+    b = test_data.TestBlock(signal.Signal(None, None, None, None, test_input))
     a.inputs[0].connect(b.outputs[0])
     if isinstance(test_input, int):
         assert a.outputs[0].data.ordinate >= 0
     if isinstance(test_input, np.ndarray):
         assert all(x >= 0 for x in a.outputs[0].data.ordinate)
-
-
--- tests/test_adder.py
+++ tests/test_adder.py
@@ -3,16 +3,7 @@ from mca.block import adder
 from mca.base import block_base
 from mca.datatypes import signal
 import numpy as np
-
-
-class TestBlock(block_base.Block):
-    def __init__(self, sig):
-        super().__init__()
-        self._new_output(meta_data=None)
-        self.outputs[0].data = sig
-
-    def _process(self):
-        pass
+import test_data
 
 
 test_signal0 = signal.Signal(None, 0, 5, 0.1, np.full((5,), 1))
@@ -33,9 +24,9 @@ test_cases = [((test_signal0, test_signal1), expected_signal0),
 @pytest.mark.parametrize("test_input, expected", test_cases)
 def test_adder_basic(test_input, expected):
     a = adder.Adder()
-    b = TestBlock(test_input[0])
+    b = test_data.TestBlock(test_input[0])
     a.inputs[0].connect(b.outputs[0])
-    c = TestBlock(test_input[1])
+    c = test_data.TestBlock(test_input[1])
     a.inputs[1].connect(c.outputs[0])
     if test_input[1] == test_signal1:
         assert a.outputs[0].data == expected
