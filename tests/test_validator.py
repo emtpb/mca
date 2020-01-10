166611b99e7395e648fd14a5421e3a0311dab0b1
Kevin Koch <kevink2@mail.uni-paderborn.de>
2020-01-10 15:56:31 +0100

    Add tests for the validator module.

---
+++ tests/test_validator.py
@@ -0,0 +1,24 @@
+import pytest
+
+from mca import exceptions
+from mca.framework import validator
+from mca.framework import data_types
+
+
+def test_check_signals():
+    validator.check_type_signal(data_types.Signal(None, None,
+                                                  None, None, None))
+    with pytest.raises(exceptions.DataTypeError):
+        validator.check_type_signal(5)
+
+
+def test_check_intervals():
+    a = data_types.Signal(None, 0, 10, 0.1, None)
+    b = data_types.Signal(None, 1, 5, 0.1, None)
+    c = data_types.Signal(None, 0.01, 5, 0.1, None)
+    d = data_types.Signal(None, 0, 10, 0.05, None)
+    validator.check_intervals([a, b])
+    with pytest.raises(exceptions.IntervalError):
+        validator.check_intervals([a, c])
+    with pytest.raises(exceptions.IntervalError):
+        validator.check_intervals([a, d])
