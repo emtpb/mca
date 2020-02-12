a5e779ca6037c45f46abf20449c95190f6e66a20
Kevin Koch <kevink2@mail.uni-paderborn.de>
2020-02-12 12:54:33 +0100

    Move path and default config from global variable to static attribute.

--- mca/config.py
+++ mca/config.py
@@ -2,24 +2,24 @@ import json
 import appdirs
 import os
 
-user_config_path = appdirs.user_config_dir('mca') + '/config.json'
-default_config = {"language": "en", }
-
 
 class Config(dict):
     """Config class of the mca. This class loads user configs and the default config into its own dict.
     It also automatically creates the config directory and file and stores all new user configs there.
     """
+    user_config_path = appdirs.user_config_dir('mca') + '/config.json'
+    default_config = {"language": "en", }
+
     def __init__(self):
         """Initialize the Config class."""
         super().__init__()
-        self.update(default_config)
+        self.update(Config.default_config)
         try:
-            with open(user_config_path, 'r') as config_file:
+            with open(Config.user_config_path, 'r') as config_file:
                 user_config = json.load(config_file)
         except FileNotFoundError:
-            os.makedirs(os.path.dirname(user_config_path), exist_ok=True)
-            with open(user_config_path, 'w') as config_file:
+            os.makedirs(os.path.dirname(Config.user_config_path), exist_ok=True)
+            with open(Config.user_config_path, 'w') as config_file:
                 json.dump({}, config_file)
                 user_config = {}
         self.update(user_config)
@@ -29,6 +29,7 @@ class Config(dict):
         that will automatically update the  corresponding config file.
         """
         super().__setitem__(key, value)
-        with open(user_config_path, 'w') as config_file:
-            user_config = {k: self[k] for k in self if k not in default_config or self[k] != default_config[k]}
+        with open(Config.user_config_path, 'w') as config_file:
+            user_config = {k: self[k] for k in self
+                           if k not in Config.default_config or self[k] != Config.default_config[k]}
             json.dump(user_config, config_file)
--- tests/test_config.py
+++ tests/test_config.py
@@ -8,10 +8,10 @@ tmp = config.Config()
 
 @pytest.fixture
 def build_cleanup_config():
-    with open(config.user_config_path, 'w') as config_file:
+    with open(config.Config.user_config_path, 'w') as config_file:
         json.dump({}, config_file)
     yield
-    with open(config.user_config_path, 'w') as config_file:
+    with open(config.Config.user_config_path, 'w') as config_file:
         json.dump({}, config_file)
     reset_config = config.Config()
     for i in tmp.keys():
@@ -19,18 +19,18 @@ def build_cleanup_config():
 
 
 def test_create_config(build_cleanup_config):
-    os.remove(config.user_config_path)
+    os.remove(config.Config.user_config_path)
     test_config = config.Config()
-    assert test_config == config.default_config
-    with open(config.user_config_path, 'r') as config_file:
+    assert test_config == config.Config.default_config
+    with open(config.Config.user_config_path, 'r') as config_file:
         assert json.load(config_file) == {}
 
 
 def test_write_config(build_cleanup_config):
     test_config = config.Config()
     test_config["test"] = "test"
-    with open(config.user_config_path, 'r') as config_file:
+    with open(config.Config.user_config_path, 'r') as config_file:
         assert json.load(config_file) == {"test": "test"}
     test_config["language"] = "en"
-    with open(config.user_config_path, 'r') as config_file:
+    with open(config.Config.user_config_path, 'r') as config_file:
         assert json.load(config_file) == {"test": "test"}
