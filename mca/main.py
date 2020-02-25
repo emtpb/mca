7d27f9f70c6d547eb18d049f41a21b45bd5f929a
Kevin Koch <kevink2@mail.uni-paderborn.de>
2020-02-25 12:02:33 +0100

    Remove change for pull request.

--- mca/main.py
+++ mca/main.py
@@ -1,9 +1,11 @@
 import sys
 from PySide2 import QtWidgets
 from mca.gui.mainwindow import MainWindow
+import os
 
 
 def main():
+    os.environ['QT_PLUGIN_PATH'] = "/upb/users/k/kevink2/profiles/unix/emt/.local/lib/python3.7/site-packages/PySide2/Qt/plugins"
     # Qt Application
     app = QtWidgets.QApplication(sys.argv)
     # QWidget
