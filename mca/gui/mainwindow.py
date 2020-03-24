048874356c5fd4dc0aa0ff4d8623277231b044fe
Kevin Koch <kevink2@mail.uni-paderborn.de>
2020-03-24 21:52:17 +0100

    Add feature to change language in gui.

--- mca/gui/mainwindow.py
+++ mca/gui/mainwindow.py
@@ -1,8 +1,11 @@
 from PySide2 import QtWidgets, QtCore
 import inspect
+import importlib
 
 import mca.blocks
 from mca.gui import block_list, block_display
+from mca import config
+from mca.language import _
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -15,15 +18,20 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.menu = self.menuBar()
         self.file_menu = self.menu.addMenu("File")
-
+        self.language_menu = self.menu.addMenu("Language")
+        languages = [("Deutsch", "de"), ("English", "en")]
+        for i in languages:
+            action = QtWidgets.QAction(i[0], self)
+            action.triggered.connect(self.change_language(i[1]))
+            self.language_menu.addAction(action)
+        restart_action = QtWidgets.QAction("Restart", self)
+        restart_action.triggered.connect(self.restart_app)
+        self.file_menu.addAction(restart_action)
         exit_action = QtWidgets.QAction("Exit", self)
         exit_action.setShortcut("Ctrl+Q")
         exit_action.setStatusTip("Close Application")
         exit_action.triggered.connect(self.exit_app)
         self.file_menu.addAction(exit_action)
-        restart_action = QtWidgets.QAction("Restart", self)
-        restart_action.triggered.connect(self.restart_app)
-        self.file_menu.addAction(restart_action)
 
         self.main_widget = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
         self.blocks = [m[1] for m in inspect.getmembers(mca.blocks, inspect.isclass)]
@@ -42,4 +50,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
     @QtCore.Slot()
     def exit_app(self):
-        QtWidgets.QApplication.quit(4)
+        QtWidgets.QApplication.quit()
+
+    def change_language(self, new_language):
+        def tmp():
+            msg_box = QtWidgets.QMessageBox()
+            msg_box.setText("Changes will be applied after restart.")
+            msg_box.exec()
+            config.Config()["language"] = new_language
+        return tmp
