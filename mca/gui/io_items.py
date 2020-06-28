50180f325d633931a7e1fea5246baefc74784f1c
Kevin Koch <kevink2@mail.uni-paderborn.de>
2020-06-28 14:28:35 +0200

    Reformat code.

--- mca/gui/io_items.py
+++ mca/gui/io_items.py
@@ -5,15 +5,16 @@ from mca.language import _
 
 
 class InputItem(QtWidgets.QGraphicsItem):
-    """Class to display an :class:`.Input` in the UI. This class supports all functionalities of :class:`.Input`.
+    """Class to display an :class:`.Input` in the UI. This class supports all
+    functionalities of :class:`.Input`.
 
     Attributes:
         width (int): Current width of the input.
         height (int): Current height of the input.
         mca_input: Reference of the :class:`.Input` to display.
         view: Reference of the :class:`.BlockView` instance.
-        connection_line: :class:`.ConnectionLine` to the :class:`.OutputItem` it is connected to. None if its not
-                         connected.
+        connection_line: :class:`.ConnectionLine` to the :class:`.OutputItem`
+                         it is connected to. None if its not connected.
         menu: Menu which pops up when the right mouse button is pressed.
         disconnect_action: Action which calls :meth:`.disconnect`.
     """
@@ -54,8 +55,8 @@ class InputItem(QtWidgets.QGraphicsItem):
         return QtCore.QRectF(0, 0, self.width, self.height)
 
     def paint(self, painter, option, widget):
-        """Method to paint the the input. This method gets invoked after initialization
-        and every time the input gets updated.
+        """Method to paint the the input. This method gets invoked after
+        initialization and every time the input gets updated.
        """
         path = QtGui.QPainterPath()
         path.moveTo(self.width, 0)
@@ -69,7 +70,8 @@ class InputItem(QtWidgets.QGraphicsItem):
         :class:`.ConnectionLine` if the input is not yet connected else the
         event is ignored.
         """
-        if self.mca_input.connected_output() or event.button() == QtCore.Qt.MouseButton.RightButton:
+        if self.mca_input.connected_output() or \
+                event.button() == QtCore.Qt.MouseButton.RightButton:
             event.ignore()
             return
         self.connection_line = ConnectionLine(event.scenePos().x(),
@@ -85,8 +87,8 @@ class InputItem(QtWidgets.QGraphicsItem):
         self.connection_line.y1 = event.scenePos().y()
 
     def mouseReleaseEvent(self, event):
-        """Attaches the connection line to an :class:`.OutputItem` if the mouse gets released above one. Otherwise
-        the connection line gets removed.
+        """Attaches the connection line to an :class:`.OutputItem` if the mouse
+        gets released above one. Otherwise the connection line gets removed.
         """
         for i in self.scene().items(event.scenePos()):
             if isinstance(i, OutputItem):
@@ -107,17 +109,23 @@ class InputItem(QtWidgets.QGraphicsItem):
         self.connection_line = None
 
     def update_connection_line(self):
-        """Method to update its connection line according to its own position."""
+        """Method to update its connection line according to its own
+        position.
+        """
         if self.connection_line:
             self.connection_line.x2 = self.scenePos().x() + 5
             self.connection_line.y2 = self.scenePos().y() + self.height / 2
 
     def contextMenuEvent(self, event):
-        """Method that is invoked when the user right-clicks the input. Opens the context menu of the input."""
+        """Method that is invoked when the user right-clicks the input.
+        Opens the context menu of the input.
+        """
         self.menu.exec_(event.screenPos())
 
     def disconnect(self):
-        """Disconnects the :class:`.InputItem` from its :class:`.OutputItem` if they are connected."""
+        """Disconnects the :class:`.InputItem` from its :class:`.OutputItem`
+        if they are connected.
+        """
         self.mca_input.disconnect()
         if self.connection_line:
             self.connection_line.output.connection_lines.remove(
@@ -127,7 +135,8 @@ class InputItem(QtWidgets.QGraphicsItem):
 
 
 class OutputItem(QtWidgets.QGraphicsItem):
-    """Class to display an :class:`.Output` in the UI. This class supports all functionalities of :class:`.Output`.
+    """Class to display an :class:`.Output` in the UI. This class supports all
+    functionalities of :class:`.Output`.
 
        Attributes:
            width (int): Current width of the output.
@@ -175,8 +184,8 @@ class OutputItem(QtWidgets.QGraphicsItem):
         return QtCore.QRectF(0, 0, self.width, self.height)
 
     def paint(self, painter, option, widget):
-        """Method to paint the output. This method gets invoked after initialization
-        and every time the output gets updated.
+        """Method to paint the output. This method gets invoked after
+        initialization and every time the output gets updated.
         """
         path = QtGui.QPainterPath()
         path.moveTo(0, 0)
@@ -186,8 +195,8 @@ class OutputItem(QtWidgets.QGraphicsItem):
         painter.fillPath(path, QtGui.QBrush(QtGui.QColor(0, 0, 0)))
 
     def mousePressEvent(self, event):
-        """Method invoked when the block gets clicked. Creates a :class:`.ConnectionLine` and
-        adds it to the connection line list.
+        """Method invoked when the block gets clicked. Creates a
+        :class:`.ConnectionLine` and adds it to the connection line list.
         """
         if event.button() == QtCore.Qt.MouseButton.RightButton:
             event.ignore()
@@ -205,8 +214,9 @@ class OutputItem(QtWidgets.QGraphicsItem):
         self.connection_lines[-1].y2 = event.scenePos().y()
 
     def mouseReleaseEvent(self, event):
-        """Attaches the last added connection line to an :class:`.InputItem` if the mouse gets released above one.
-        Otherwise the last added connection line gets removed.
+        """Attaches the last added connection line to an :class:`.InputItem`
+        if the mouse gets released above one. Otherwise the last added
+        connection line gets removed.
         """
         for i in self.scene().items(event.scenePos()):
             if isinstance(i, InputItem):
@@ -226,13 +236,17 @@ class OutputItem(QtWidgets.QGraphicsItem):
         self.scene().removeItem(self.connection_lines.pop(-1))
 
     def update_connection_line(self):
-        """Method to update all its connection lines according to its own position."""
+        """Method to update all its connection lines according to its own
+        position.
+        """
         for connection_line in self.connection_lines:
             connection_line.x1 = self.scenePos().x() + 5
             connection_line.y1 = self.scenePos().y() + self.height / 2
 
     def disconnect(self):
-        """Disconnects the :class:`.OutputItem` from all its :class:`.InputItem` s."""
+        """Disconnects the :class:`.OutputItem` from all its
+        :class:`.InputItem` s.
+        """
         self.mca_output.disconnect()
         for connection_line in self.connection_lines:
             connection_line.input.connection_line = None
@@ -240,12 +254,15 @@ class OutputItem(QtWidgets.QGraphicsItem):
         self.connection_lines = []
 
     def contextMenuEvent(self, event):
-        """Method that is invoked when the user right-clicks the output. Opens the context menu of the output."""
+        """Method that is invoked when the user right-clicks the output.
+        Opens the context menu of the output.
+        """
         self.menu.exec_(event.screenPos())
 
 
 class ConnectionLine(QtWidgets.QGraphicsLineItem):
-    """Class which represents the connection between an :class:`.Input` and an :class:`.Output`.
+    """Class which represents the connection between an :class:`.Input` and
+    an :class:`.Output`.
 
     Attributes:
         output: :class:`.OutputItem` which is part of the connection.
