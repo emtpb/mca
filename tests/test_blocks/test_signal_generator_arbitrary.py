3aa37565e51ede527ecde9c7ac543ecbdb31febd
Kevin Koch <kevink2@mail.uni-paderborn.de>
2020-07-06 11:40:15 +0200

    Define file path.

--- tests/test_blocks/test_signal_generator_arbitrary.py
+++ tests/test_blocks/test_signal_generator_arbitrary.py
@@ -21,20 +21,18 @@ def test_load_json(sin_signal):
                  "values": sin_signal.values,
                  "increment": sin_signal.increment,
                  "ordinate": str(sin_signal.ordinate)}
-
-    with open(dir_path+"/sin.json", "w") as save_file:
+    file_path = dir_path+"/sin.json"
+    with open(file_path, "w") as save_file:
         json.dump(test_dict, save_file)
     b = signal_generator_arbitrary.SignalGeneratorArbitrary()
-    b.load_data(dir_path+"/sin.json")
+    b.load_data(file_path)
     assert b.outputs[0].data == sin_signal
     assert b.outputs[0].data.meta_data == sin_signal.meta_data
     with pytest.raises(FileNotFoundError):
         b.load_data("test")
     test_dict["data_type"] = "test"
-    with open(dir_path + "/sin.json", "w") as save_file:
+    with open(file_path, "w") as save_file:
         json.dump(test_dict, save_file)
     with pytest.raises(exceptions.DataLoadingError):
-        b.load_data(dir_path+"/sin.json")
-    file_list = [f for f in os.listdir(dir_path) if f.endswith(".json")]
-    for f in file_list:
-        os.remove(os.path.join(dir_path, f))
+        b.load_data(file_path)
+    os.remove(file_path)
