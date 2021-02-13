ffbe6f77059bfb13601552f8428e5c63df97026e
Kevin Koch <kevink2@mail.uni-paderborn.de>
2021-02-13 11:11:24 -0800

    Add AudioInput block.

--- mca/blocks/__init__.py
+++ mca/blocks/__init__.py
@@ -11,6 +11,7 @@ from .absolute import Absolute
 from .signal_generator_arbitrary import SignalGeneratorArbitrary
 from .signal_saver import SignalSaver
 from .audio_output import AudioOutput
+from .audio_input import AudioInput
 
 block_classes = [i[1] for i in inspect.getmembers(sys.modules[__name__],
                                                   inspect.isclass)]
