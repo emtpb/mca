Fix bug Units are falsely computed.

mca/blocks/cpsd.py
@@ -15,7 +15,7 @@ class CrossPowerDensitySpectrum(Block):
         self._new_output(meta_data=data_types.MetaData(
                 "",
                 unit_a="1/s",
-                unit_o="V*V",
+                unit_o="V*V*s",
                 quantity_a=_("Frequency")
         ))
         self._new_input()
@@ -39,7 +39,7 @@ class CrossPowerDensitySpectrum(Block):
         cpsd = np.fft.fft(ccf)
         abscissa_start = 0
         values = first_signal.values + second_signal.values - 1
-        unit_o = first_signal.meta_data.unit_o * second_signal.meta_data.unit_o
+        unit_o = first_signal.meta_data.unit_o * second_signal.meta_data.unit_o * first_signal.meta_data.unit_a
         unit_a = 1/first_signal.meta_data.unit_a
         meta_data = data_types.MetaData(None, unit_a, unit_o)
         increment = 1 / (

mca/blocks/fftplot.py
@@ -74,6 +74,9 @@ class FFTPlot(Block):
         ordinate = np.fft.fft(input_signal.ordinate)
         if normalize:
             ordinate = ordinate/values
+            unit_o = input_signal.meta_data.unit_o
+        else:
+            unit_o = input_signal.meta_data.unit_a * input_signal.meta_data.unit_o
         abscissa = np.linspace(0, delta_f*(values-1), values)
         # Apply parameters
         if shift == "shift" or \
@@ -103,9 +106,7 @@ class FFTPlot(Block):
             ordinate = np.angle(ordinate)
         meta_data = data_types.MetaData(input_signal.meta_data.name,
                                         unit_a=1/input_signal.meta_data.unit_a,
-                                        unit_o=input_signal.meta_data.unit_o,
-                                        quantity_o=input_signal.meta_data.quantity_o,
-                                        symbol_o=input_signal.meta_data.symbol_o
+                                        unit_o=unit_o,
                                         )
         label = input_signal.meta_data.name
         self.axes.plot(abscissa, ordinate, label=label)