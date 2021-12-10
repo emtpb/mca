33fcf25650f762493509605b4b758e4489444127
Kevin Koch <kevink2@mail.uni-paderborn.de>
2021-12-10 21:23:32 +0100

    Update cli
    Command line interface can return the version.

--- mca/main.py
+++ mca/main.py
@@ -1,5 +1,7 @@
 import argparse
+import os
 
+import mca
 from mca.gui.pyside2 import main as pyside2_main
 
 
@@ -8,11 +10,20 @@ def main():
     GUI to start.
     """
     parser = argparse.ArgumentParser(description='Execute the Multi Channel Analyzer')
-    parser.add_argument("--pyside2", "-g", type=str, nargs=1, choices=["pyside2"],
-                        help='Choose the pyside2 for the mca', default="pyside2")
-
+    parser.add_argument("-g", "--gui", type=str, choices=["pyside2"],
+                        help='Choose the gui for the mca', default="pyside2")
+    parser.add_argument("-V", "--version", help='Get the version',
+                        action='store_true', dest="version")
     args = vars(parser.parse_args())
-    if args["pyside2"] == "pyside2":
+
+    if args["version"]:
+        mca_path = os.path.abspath(os.path.dirname(mca.__file__))
+        with open(os.path.join(mca_path, 'version.txt')) as version_file:
+            version = version_file.read()
+        print("Multi Channel Analyzer Version: {}".format(version))
+        return
+
+    if args["gui"] == "pyside2":
         pyside2_main.main()
 
 
