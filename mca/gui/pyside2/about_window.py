71b1f4516d526184c3ef394ed3610410109ce8e7
Kevin Koch <kevink2@mail.uni-paderborn.de>
2021-12-10 20:59:46 +0100

    Add version file

--- mca/gui/pyside2/about_window.py
+++ mca/gui/pyside2/about_window.py
@@ -1,6 +1,7 @@
 from PySide2 import QtWidgets, QtCore, QtGui
 import os
 
+import mca
 from mca.language import _
 
 
@@ -20,10 +21,10 @@ class AboutWindow(QtWidgets.QDialog):
             parent: Parent of this window.
         """
         QtWidgets.QDialog.__init__(self, parent=parent)
-        self.setFixedSize(510, 380)
+        self.setFixedSize(510, 430)
         # Create button box
         self.button_box = QtWidgets.QDialogButtonBox(self)
-        self.button_box.setGeometry(QtCore.QRect(160, 330, 341, 32))
+        self.button_box.setGeometry(QtCore.QRect(160, 385, 341, 32))
         self.button_box.setOrientation(QtCore.Qt.Horizontal)
         self.button_box.setStandardButtons(QtWidgets.QDialogButtonBox.Close)
         self.button_box.button(QtWidgets.QDialogButtonBox.Close).setText(_("Close"))
@@ -35,30 +36,46 @@ class AboutWindow(QtWidgets.QDialog):
         self.emt_logo.setScaledContents(False)
         # Create widget for the info
         self.about_info = QtWidgets.QWidget(self)
-        self.about_info.setGeometry(QtCore.QRect(20, 260, 726, 61))
+        self.about_info.setGeometry(QtCore.QRect(20, 300, 726, 90))
         self.about_info_layout = QtWidgets.QFormLayout(self.about_info)
         # Create labels
         font = QtGui.QFont()
         font.setPointSize(13)
-        label_1 = QtWidgets.QLabel(self.about_info)
-        label_1.setText(_("Developed by:"))
-        label_1.setFont(font)
-        label_2 = QtWidgets.QLabel(self.about_info)
-        label_2.setText(_("Measurement Engineering Group"))
-        label_2.setFont(font)
-        label_3 = QtWidgets.QLabel(self.about_info)
-        label_3.setText(_("Homepage:"))
-        label_3.setFont(font)
-        label_4 = QtWidgets.QLabel(self.about_info)
-        label_4.setText(_('<a href="https://ei.uni-paderborn.de/en/electrical-engineering/emt/electrical-engineering/home">EMT</a>'))
-        label_4.setOpenExternalLinks(True)
-        label_4.setFont(font)
-        # Add labels
 
-        self.about_info_layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, label_1)
-        self.about_info_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, label_2)
-        self.about_info_layout.setWidget(1, QtWidgets.QFormLayout.LabelRole, label_3)
-        self.about_info_layout.setWidget(1, QtWidgets.QFormLayout.FieldRole, label_4)
+        develop_label = QtWidgets.QLabel(self.about_info)
+        develop_label.setText(_("Developed by:"))
+        develop_label.setFont(font)
+
+        develop_field_label = QtWidgets.QLabel(self.about_info)
+        develop_field_label.setText(_("Measurement Engineering Group"))
+        develop_field_label.setFont(font)
+
+        homepage_label = QtWidgets.QLabel(self.about_info)
+        homepage_label.setText(_("Homepage:"))
+        homepage_label.setFont(font)
+
+        homepage_field_label = QtWidgets.QLabel(self.about_info)
+        homepage_field_label.setText(_('<a href="https://ei.uni-paderborn.de/en/electrical-engineering/emt/electrical-engineering/home">EMT</a>'))
+        homepage_field_label.setOpenExternalLinks(True)
+        homepage_field_label.setFont(font)
+
+        version_label = QtWidgets.QLabel(self.about_info)
+        version_label.setText(_("Version:"))
+        version_label.setFont(font)
+
+        mca_path = os.path.abspath(os.path.dirname(mca.__file__))
+        with open(os.path.join(mca_path, 'version.txt')) as version_file:
+            version = version_file.read()
+        version_field_label = QtWidgets.QLabel(self.about_info)
+        version_field_label.setText(version)
+        version_field_label.font()
+        # Add labels
+        self.about_info_layout.setWidget(0, QtWidgets.QFormLayout.LabelRole, develop_label)
+        self.about_info_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, develop_field_label)
+        self.about_info_layout.setWidget(1, QtWidgets.QFormLayout.LabelRole, homepage_label)
+        self.about_info_layout.setWidget(1, QtWidgets.QFormLayout.FieldRole, homepage_field_label)
+        self.about_info_layout.setWidget(2, QtWidgets.QFormLayout.LabelRole, version_label)
+        self.about_info_layout.setWidget(2, QtWidgets.QFormLayout.FieldRole, version_field_label)
 
         self.button_box.accepted.connect(self.accept)
         self.button_box.rejected.connect(self.reject)
