e646014e11273e68b9ae99064c67938f154d2d8c
Kevin Koch <kevink2@mail.uni-paderborn.de>
2022-01-25 17:42:55 +0100

    Add normalize parameter

--- mca/blocks/audio_loader.py
+++ mca/blocks/audio_loader.py
@@ -1,4 +1,5 @@
 import scipy.io.wavfile
+import numpy as np
 
 from mca.framework import parameters, Block, data_types
 from mca.language import _
@@ -13,12 +14,11 @@ class AudioLoader(Block):
 
     def setup_parameters(self):
         self.parameters.update(
-            {"sampling_freq": parameters.IntParameter(_("Sampling frequency"),
-                                                      0, None, "Hz", 44100),
-             "file_name": parameters.PathParameter(_("Filename"), [".wav"],
-                                                  loading=True),
+            {"file_name": parameters.PathParameter(_("Filename"), [".wav"],
+                                                   loading=True),
              "load_file": parameters.ActionParameter(_("Load file"),
                                                      self.load_wav),
+             "normalize": parameters.BoolParameter(_("Normalize"), default=True)
              })
 
     def setup_io(self):
@@ -34,12 +34,15 @@ class AudioLoader(Block):
     def load_wav(self):
         """Reads a .wav and puts the data on the first output."""
         filename = self.parameters["file_name"].value
+        normalize = self.parameters["normalize"].value
         if not filename.endswith(".wav"):
             raise exceptions.DataLoadingError("File has to be a .wav")
         try:
             rate, data = scipy.io.wavfile.read(filename)
         except FileNotFoundError:
             raise exceptions.DataLoadingError("File not found")
+        if normalize:
+            data = data/np.max(data)
         self.outputs[0].data = data_types.Signal(
             metadata=self.outputs[0].metadata,
             abscissa_start=0,
