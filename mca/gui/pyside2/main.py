96bdf01873335184bf2486b8b623d39c5b5ce151
Kevin Koch <kevink2@mail.uni-paderborn.de>
2022-04-22 17:05:54 +0200

    Set environmental variable for Qt plugins

--- mca/gui/pyside2/main.py
+++ mca/gui/pyside2/main.py
@@ -1,15 +1,24 @@
-from PySide2 import QtWidgets
+import PySide2
+import os
+import platform
 import sys
 import matplotlib
 
-from mca.gui.pyside2 import main_window
+pyside_path = os.path.dirname(PySide2.__file__)
+if platform.system() == "Linux":
+    plugin_path = os.path.join(pyside_path, "Qt", "plugins")
+    os.environ["QT_PLUGIN_PATH"] = plugin_path
+elif platform.system() == "Windows":
+    plugin_path = os.path.join(pyside_path, "plugins")
+    os.environ["QT_PLUGIN_PATH"] = plugin_path
 
 
 def main():
     """Main function for the PySide2 GUI of mca. Switches the matplotlib
     backend to Qt5 and opens the :class:`.MainWindow` ."""
+    from mca.gui.pyside2 import main_window
     matplotlib.use("qt5agg")
-    app = QtWidgets.QApplication(sys.argv)
+    app = PySide2.QtWidgets.QApplication(sys.argv)
     window = main_window.MainWindow()
     window.show()
     app.exec_()
