ec38e3bd5fbd89b4b8af81c885780093d85477bb
Kevin Koch <kevink2@mail.uni-paderborn.de>
2023-06-19 14:20:32 +0200

    Add SignalGenerator block

--- mca/blocks/__init__.py
+++ mca/blocks/__init__.py
@@ -39,6 +39,7 @@ from .quantization import Quantization
 from .real_to_complex import RealToComplex
 from .resample import Resample
 from .signal_loader import SignalLoader
+from .signal_generator import SignalGenerator
 from .signal_generator_periodic import SignalGeneratorPeriodic
 from .signal_generator_stochastic import SignalGeneratorStochastic
 from .signal_saver import SignalSaver
