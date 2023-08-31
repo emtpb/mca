8836c57a607e152775b2fad4ea3ab9e7fdf85dcb
Kevin Koch <kevink2@mail.uni-paderborn.de>
2023-08-31 12:10:28 +0200

    Add triggers
    Parameters can now trigger actions when values change.

--- mca/blocks/audio_loader.py
+++ mca/blocks/audio_loader.py
@@ -28,6 +28,7 @@ class AudioLoader(Block):
         self.parameters["load_file"] = parameters.ActionParameter(
             name="Load file", function=self.load_wav
         )
+        self.parameters["file_name"].triggers = [self.parameters["load_file"]]
         self.parameters["normalize"] = parameters.BoolParameter(
             name="Normalize", default=True
         )
